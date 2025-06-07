e2720c5cde6a9b24285f277180074e1bc1675ac7
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2025-06-07 11:09:21 +0700

    echo builtin

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,18 @@
+def parse_command(command, args)
+    case command
+    when "echo"
+        words = args.join(' ')
+        $stdout.write("#{words}\n")
+    when "exit"
+        exit 0 if args[0] == "0"
+    else
+        $stdout.write("#{command}: command not found\n")
+    end
+end
+
 # Wait for user input
 while true do
     $stdout.write("$ ")
     command, *args = gets.chomp.split(" ")
-
-    exit 0 if command == "exit" && args[0] == "0"
-    $stdout.write("#{command}: command not found\n")
+    parse_command(command, args)
 end
