30cc7fcbf5cfee2cd2a58647cfacfbafc0291cb1
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2025-06-07 11:24:59 +0700

    builtins

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,13 @@
+@builtin_command = ["echo", "exit", "type"]
+
 def parse_command(command, args)
     case command
+    when "type"
+        if @builtin_command.include?(args[0])
+            $stdout.write("#{args[0]} is a shell builtin\n")
+        else
+            $stdout.write("#{args[0]}: not found\n")
+        end
     when "echo"
         words = args.join(' ')
         $stdout.write("#{words}\n")
