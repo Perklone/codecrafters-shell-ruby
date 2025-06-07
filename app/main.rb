104f234d84fd65ee231defa511e3b1a8f378b482
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2025-06-07 11:00:14 +0700

    exit builtin

--- app/main.rb
+++ app/main.rb
@@ -2,5 +2,7 @@
 while true do
     $stdout.write("$ ")
     command, *args = gets.chomp.split(" ")
+
+    exit 0 if command == "exit" && args[0] == "0"
     $stdout.write("#{command}: command not found\n")
 end
